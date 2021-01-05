ab790f0a6dba5dc407c666b90629cb1a9d046419
MarioBrossBlack <crossholly4872@gmail.com>
2021-01-05 11:07:49 +0700

    menu dan form siswa

--- config.php
+++ config.php
@@ -6,8 +6,9 @@ $password = "";
 $nama_database = "pendaftaran_siswa"
 
 $db = mysqli_connect($server, $user, $password, $nama_database);
+
 if( !$db ){
-    die("Gagal Terhubung dengan database: " . mysqli_connect_error());
+    die("Gagal terhubung dengan database: " . mysqli_connect_error());
 }
 
 ?>
