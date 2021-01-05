e73aa1f287be5c1ad5d8e94d640591b9265aedec
MarioBrossBlack <crossholly4872@gmail.com>
2021-01-05 10:26:23 +0700

    menu utama

---
+++ index.php
@@ -0,0 +1,17 @@
+<html>
+<head>
+<title>Pendaftaran Siswa Baru</title>
+</head>
+<body>
+    <header>
+    <h3>Pendaftaran Siswa Baru</h3>
+    </header>
+    <h4>Menu</h4>
+    <nav>
+    <ul>
+    <li><a href="form-daftar.php">Daftar Baru</a></li>
+    <li><a href="list-siswa.php">Pendaftaran</a></li>
+    </ul>
+    </nav>
+</body>
+</html>
